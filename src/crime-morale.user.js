6bf4a13a21dad38668e3bf4f28fc9fa7e261aaad
tobytorn <1617955@torn.com>
2024-10-13 23:21:39 +0800

    Bump version to 1.4.2

--- src/crime-morale.user.js
+++ src/crime-morale.user.js
@@ -4,7 +4,7 @@
 // @description tobytorn 自用 Crime 2.0 助手
 // @author      tobytorn [1617955]
 // @match       https://www.torn.com/loader.php?sid=crimes*
-// @version     1.4.1
+// @version     1.4.2
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       unsafeWindow
